85d6c32cedcd87909e83ed1e51bbdd89100ba97c
Victorhu1912 <86376677+Victorhu1912@users.noreply.github.com>
2021-06-28 23:51:54 +1000

    Create Assign7(admin).php

---
+++ Assignment/Assign7(admin).php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<h1>Admin Mode</h1>
+<p></p>
+<a href="Assign7(admin1).php">Add a record</a>
+<p></p>
+<a href="Assign7(admin2).php">Update a record</a>
+<p></p>
+<a href="Assign7(admin3).php">Delete a record</a>
+<p></p>
+
+<form name="f3" action="log.php">
+<input type = "submit" value="Download record log"/>
+</form>
+
+<br /><br />
+<a href="Assign7.php">Back to Web Home</a>
+ 
+</body>
+ </html> 
